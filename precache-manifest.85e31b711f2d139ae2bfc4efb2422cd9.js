f545ae68f7f9ef8fc48f5819047bc94cde4e5cd7
hernandoagf <22449631+hernandoagf@users.noreply.github.com>
2020-07-26 20:17:02 -0300

    Updates

---
+++ precache-manifest.85e31b711f2d139ae2bfc4efb2422cd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb4ade191dad9ef8e0541402bf3054a",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "4f8b51e1fd996f36edad",
+    "url": "/random-quote-machine/static/css/main.a2472e8d.chunk.css"
+  },
+  {
+    "revision": "3e8127a59aaa597d40ea",
+    "url": "/random-quote-machine/static/js/2.6193ef56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.6193ef56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f8b51e1fd996f36edad",
+    "url": "/random-quote-machine/static/js/main.c32b35b9.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  }
+]);
